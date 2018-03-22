327c01b66d332d7bebc98899dce080938939b629
Rafal Swieton <rafal@eskrzynka.pl>
2018-03-23 00:28:48 +0100

    Add index.js, handlers.js, server.js, start.html, upload.html

---
+++ modules/handlers.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var formidable = require('formidable');
+var newpath;
+
+
+exports.upload = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania upload.");
+    var form = new formidable.IncomingForm();
+
+    form.parse(request, function(error, fields, files) {
+    newpath = './files_uploaded/' + files.upload.name;
+    fs.renameSync(files.upload.path, newpath);
+    fs.readFile('templates/upload.html', function(err, html) {
+    	response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    	response.write(html);
+    	response.write("Zapisano plik: " + files.upload.name + "<br/>");
+    	response.write("<img src='/show' />");
+    	response.end();
+    });
+    });
+}
+
+exports.welcome = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania welcome.");
+    fs.readFile('templates/start.html', function(err, html) {
+    	response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    	response.write(html);
+    	response.end();	
+    });
+}
+
+exports.show = function(request, response) {
+	fs.readFile(newpath, "binary", function(error, file) {
+		response.writeHead(200, {"Content-Type": "image/png"});
+		response.write(file, "binary");
+		response.end();
+	});
+}
+
+exports.style = function(request, response) {
+    fs.readFile('templates/style.css', function(err, css) {
+    	response.writeHead(200, {"Content-Type": "text/css"});
+    	response.write(css);
+    	response.end();
+    });
+}
+
+exports.error = function(request, response) {
+    console.log("Nie wiem co robić.");
+    response.write("404 :(");
+    response.end();
+}
